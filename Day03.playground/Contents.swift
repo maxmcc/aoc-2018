e6a8c0ace34fc6aae95346ba0e44cfd3034301f9
Max McCarthy <hello@maxmcc.com>
2018-12-02 22:18:09 -0800

    Add implementation for day 3

---
+++ Day03.playground/Contents.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+extension Sequence {
+    
+    func count(where predicate: (Element) throws -> Bool) rethrows -> Int {
+        var count = 0
+        for e in self {
+            if try predicate(e) {
+                count += 1
+            }
+        }
+        return count
+    }
+}
+
+public struct Claim {
+    var id: Int
+    var horizontalExtent: Range<Int>
+    var verticalExtent: Range<Int>
+    
+    init<S>(_ string: S) where S: StringProtocol {
+        self.id = 0
+        var x = 0
+        var y = 0
+        var width = 0
+        var height = 0
+        
+        let scanner = Scanner(string: String(string))
+        scanner.charactersToBeSkipped = CharacterSet.decimalDigits.inverted
+        scanner.scanInt(&self.id)
+        scanner.scanInt(&x)
+        scanner.scanInt(&y)
+        scanner.scanInt(&width)
+        scanner.scanInt(&height)
+        
+        self.horizontalExtent = x..<(x + width)
+        self.verticalExtent = y..<(y + height)
+    }
+}
+
+
+let inputURL = Bundle.main.url(forResource: "Input", withExtension: "txt")!
+let inputString = try! String(contentsOf: inputURL, encoding: .utf8)
+let inputLines = inputString.split(separator: "\n")
+
+
+let claims = inputLines.map(Claim.init)
+
+var squares = Array(repeating: Array(repeating: Set<Int>(), count: 1000), count: 1000)
+
+for claim in claims {
+    for rowIndex in claim.verticalExtent {
+        for columnIndex in claim.horizontalExtent {
+            squares[rowIndex][columnIndex].insert(claim.id)
+        }
+    }
+}
+
+let totalOverlapped = squares.reduce(0) { count, row in
+    count + row.count { cell in cell.count > 1 }
+}
+
+print("Part 1:", totalOverlapped)
+
+var isSafe = Array(repeating: true, count: claims.count + 1)
+
+for row in squares {
+    for impingingClaims in row {
+        if impingingClaims.count > 1 {
+            for claimID in impingingClaims {
+                isSafe[claimID] = false
+            }
+        }
+    }
+}
+
+let safeClaim = isSafe.dropFirst().firstIndex(of: true)!
+
+print("Part 2:", safeClaim)
